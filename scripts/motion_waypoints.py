d3e177871b1f825cae128549c0207b0e4354d063
ivxvm <ivxvm@protonmail.com>
2024-12-07 17:23:34 +0200

    Implement splines (doesn't work well sadly)

--- scripts/motion_waypoints.py
+++ scripts/motion_waypoints.py
@@ -4,8 +4,9 @@
 #   | start type = on trigger, autoloop = true + empty object with trigger on enter parented to platform
 
 
-import bge, bpy
+import bge, bpy, math
 from collections import OrderedDict
+from mathutils import Vector
 
 STATE_INIT = "INIT"
 STATE_RUNNING = "RUNNING"
@@ -56,6 +57,50 @@ class MotionWaypoints(bge.types.KX_PythonComponent):
             self.travel_times_by_segment.append((segment_lengths[i] / total_distance) * self.total_travel_time)
         print("self.travel_times_by_segment", self.travel_times_by_segment)
 
+    def compute_splines(self):
+        n = len(self.waypoints)
+        xs = []
+        ys = []
+        for i in range(n):
+            w = self.waypoints[i]
+            p = w.worldPosition
+            xs.append(p.x)
+            ys.append(p.y)
+        coeffs_a = []
+        coeffs_h = []
+        for i in range(n):
+            coeffs_a.append(ys[i])
+            if i < n - 1:
+                coeffs_h.append(xs[i + 1] - xs[i])
+        coeffs_alpha = [0]
+        for i in range(1, n - 1):
+            coeffs_alpha.append(
+                3 * (coeffs_a[i + 1] - coeffs_a[i]) / coeffs_h[i] -
+                3 * (coeffs_a[i] - coeffs_a[i - 1]) / coeffs_h[i - 1])
+        coeffs_l = [1]
+        coeffs_u = [0]
+        coeffs_z = [0]
+        for i in range(1, n - 1):
+            coeffs_l.append(2 * (xs[i + 1] - xs[i - 1]) - coeffs_h[i - 1] * coeffs_u[i - 1])
+            coeffs_u.append(coeffs_h[i] / coeffs_l[i])
+            coeffs_z.append((coeffs_alpha[i] - coeffs_h[i - 1] * coeffs_z[i - 1]) / coeffs_l[i])
+        coeffs_c = [0 for _ in range(n)]
+        coeffs_b = [0 for _ in range(n)]
+        coeffs_d = [0 for _ in range(n)]
+        for j in reversed(range(n - 1)):
+            coeffs_c[j] = coeffs_z[j] - coeffs_u[j] * coeffs_c[j + 1]
+            if coeffs_h[j] == 0:
+                coeffs_b[j] = 0
+                coeffs_d[j] = 0
+            else:
+                coeffs_b[j] = (coeffs_a[j + 1] - coeffs_a[j]) / coeffs_h[j] - coeffs_h[j] * (coeffs_c[j + 1] + 2 * coeffs_c[j]) / 3
+                coeffs_d[j] = (coeffs_c[j + 1] - coeffs_c[j]) / (3 * coeffs_h[j])
+        self.splines = []
+        for i in range(n):
+            self.splines.append([coeffs_a[i], coeffs_b[i], coeffs_c[i], coeffs_d[i], xs[i]])
+        # print("splines = ", len(self.splines))
+        # print(self.splines)
+
     def is_end_of_path(self):
         if self.is_cycle:
             return False
@@ -104,16 +149,33 @@ class MotionWaypoints(bge.types.KX_PythonComponent):
         timestamp = bge.logic.getClockTime()
         if self.state == STATE_INIT:
             self.init_waypoints()
+            self.compute_splines()
             self.state = STATE_RUNNING
             if self.start_type == START_TYPE_ON_LOAD:
                 self.trigger()
         elif self.state == STATE_RUNNING:
             if self.is_active:
                 current_time = bge.logic.getClockTime()
-                progress = (current_time - self.start_time) / self.travel_times_by_segment[self.current_segment_index]
+                progress = min(1, (current_time - self.start_time) / self.travel_times_by_segment[self.current_segment_index])
+                # if self.object.name == "DeathStar":
+                    # print("progress", progress, self.travel_times_by_segment[self.current_segment_index])
                 displacement = self.displacements[self.current_segment_index] * self.direction_sign
-                self.object.worldPosition = self.start_position + displacement * progress
+                # self.object.worldPosition = self.start_position + displacement * progress
+                new_linear_position = self.start_position + displacement * progress
+                lpx = new_linear_position.x
+                [a, b, c, d, x] = self.splines[self.current_segment_index]
+                spline_y1 = a + b * (lpx - x) + c * math.pow(lpx - x, 2) + d * math.pow(lpx - x, 3)
+                [a, b, c, d, x] = self.splines[self.current_segment_index + 1]
+                spline_y2 = a + b * (lpx - x) + c * math.pow(lpx - x, 2) + d * math.pow(lpx - x, 3)
+                spline_y = spline_y1 * (1 - progress) + spline_y2 * progress
+                self.object.worldPosition = Vector([lpx, spline_y, new_linear_position.z])
+                if self.object.name == "DeathStar":
+                    print("spline_y1/2", spline_y1, spline_y2)
+                    print("xy", self.object.worldPosition.x, self.object.worldPosition.y,)
+
                 if current_time >= self.end_time:
+                    if self.object.name == "DeathStar":
+                        print("OOOOPS")
                     if self.is_end_of_path() and self.delay > 0.0:
                         self.state = STATE_DELAY
                         self.delay_elapsed = 0.0
